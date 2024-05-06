e48f04c4ffdb1e9ee3742d0db23b9690293c3228
scerbikf <filipscerbik487@gmail.com>
2024-05-06 12:32:19 +0200

    final

--- my-react-app/src/About.jsx
+++ my-react-app/src/About.jsx
@@ -6,17 +6,18 @@ function About(){
     const barHTML = useRef(null);
     const barCSS = useRef(null);
     const barJS = useRef(null);
+    const barSASS = useRef(null);
     const barTW = useRef(null);
     const barBT = useRef(null);
     const barREACT = useRef(null);
     const barPHP = useRef(null);
+    const barSQL = useRef(null);
     const barPYTHON = useRef(null);
     const barJAVA = useRef(null);
-    const barSQL = useRef(null);
 
     useEffect(() => {
 
-        if(barHTML, barCSS, barJS, barTW, barBT, barREACT, barPHP, barPYTHON, barJAVA, barSQL) {
+        if(barHTML, barCSS, barJS, barSASS, barTW, barBT, barREACT, barPHP, barSQL, barPYTHON, barJAVA) {
           const active = () => {
             if(window.innerWidth > 1100 ? window.scrollY > 600 : window.scrollY > 1100){
                 barHTML.current.style.width = "95%"
@@ -24,20 +25,22 @@ function About(){
                 barCSS.current.style.width = "95%"
                 barJS.current.style.transition = "1.5s 0.6s width ease-in-out"
                 barJS.current.style.width = "95%"
-                barTW.current.style.transition = "1.5s 0.8s width ease-in-out"
+                barSASS.current.style.transition = "1.5s 0.6s width ease-in-out"
+                barSASS.current.style.width = "90%"
+                barTW.current.style.transition = "1.5s 1s width ease-in-out"
                 barTW.current.style.width = "90%"
-                barBT.current.style.transition = "1.5s 1s width ease-in-out"
+                barBT.current.style.transition = "1.5s 1.2s width ease-in-out"
                 barBT.current.style.width = "90%"
-                barREACT.current.style.transition = "1.5s 1.2s width ease-in-out"
+                barREACT.current.style.transition = "1.5s 1.4s width ease-in-out"
                 barREACT.current.style.width = "85%"
-                barPHP.current.style.transition = "1.5s 1.4s width ease-in-out"
-                barPHP.current.style.width = "50%"
-                barPYTHON.current.style.transition = "1.5s 1.6s width ease-in-out"
-                barPYTHON.current.style.width = "50%"
-                barJAVA.current.style.transition = "1.5s 1.8s width ease-in-out"
-                barJAVA.current.style.width = "40%"
-                barSQL.current.style.transition = "1.5s 2s width ease-in-out"
-                barSQL.current.style.width = "30%"
+                barPHP.current.style.transition = "1.5s 1.6s width ease-in-out"
+                barPHP.current.style.width = "75%"
+                barSQL.current.style.transition = "1.5s 1.8s width ease-in-out"
+                barSQL.current.style.width = "70%"
+                barPYTHON.current.style.transition = "1.5s 2s width ease-in-out"
+                barPYTHON.current.style.width = "60%"
+                barJAVA.current.style.transition = "1.5s 2.2s width ease-in-out"
+                barJAVA.current.style.width = "55%"
             }
           }
           window.addEventListener("scroll", active)
@@ -73,6 +76,11 @@ function About(){
                                 <div ref={barJS} className="progress-bar2"></div>                       
                             </div> 
 
+                            <p>SASS</p>  
+                            <div className="progress">
+                                <div ref={barSASS} className="progress-bar2"></div>                       
+                            </div>
+
                             <p>Tailwind</p> 
                             <div className="progress">
                                 <div ref={barTW} className="progress-bar2"></div>                       
@@ -88,10 +96,14 @@ function About(){
                                 <div ref={barREACT} className="progress-bar2"></div>                       
                             </div> 
 
-
                             <p>PHP</p> 
                             <div className="progress">
                                 <div ref={barPHP} className="progress-bar2"></div>                       
+                            </div> 
+
+                            <p>SQL</p> 
+                            <div className="progress">
+                                <div ref={barSQL} className="progress-bar2"></div>                       
                             </div> 
 
                             <p>Python</p> 
@@ -103,11 +115,6 @@ function About(){
                             <div className="progress">
                                 <div ref={barJAVA} className="progress-bar2"></div>                       
                             </div>
-
-                            <p>SQL</p> 
-                            <div className="progress">
-                                <div ref={barSQL} className="progress-bar2"></div>                       
-                            </div> 
                 
                     </div>
                 </div>
